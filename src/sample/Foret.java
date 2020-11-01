db75c6f203aa2600e7ad13e3211685c265791b60
matthieu-giaccaglia <matthieu.giaccaglia@etu.umontpellier.fr>
2020-11-01 18:58:03 +0100

    addFils

--- src/sample/Foret.java
+++ src/sample/Foret.java
@@ -1,19 +1,20 @@
 package sample;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Foret {
 
     private int nbTour;
     private int taille;
-    private Arbre[][] listeArbre;
+    private Arbre[][] tableauArbre;
     private ArrayList<Arbre> list = new ArrayList<>();
     
 
     public Foret(int nbTour, int taille) {
         this.nbTour = nbTour;
         this.taille = taille;
-        this.listeArbre = new Arbre[taille][taille];
+        this.tableauArbre = new Arbre[taille][taille];
     }
 
     public int getNbTour(){
@@ -30,34 +31,48 @@ public class Foret {
         return list;
     }
 
-    public void addArbre(Arbre arbrePere) {
-        list.add(arbrePere);
-        listeArbre[arbrePere.getX()][arbrePere.getY()] = arbrePere;
+    public void addArbre(Arbre arbreAjoute) {
+        list.add(arbreAjoute);
+        tableauArbre[arbreAjoute.getX()][arbreAjoute.getY()] = arbreAjoute;
         listToString();
     }
 
     public void deleteArbre(int index){
         Arbre arbre = list.get(index);
-        listeArbre[arbre.getX()][arbre.getY()] = null;
+        tableauArbre[arbre.getX()][arbre.getY()] = null;
         list.remove(list.get(index));
         listToString();
     }
 
     public void addFils(Arbre arbrePere) {
-        boolean arbrePlante = false;
-        int x = arbrePere.getX();
-        int y = arbrePere.getY();
 
-        while(!arbrePlante) {
 
-            if (listeArbre[x][y] == null) {
-                // listeArbre[x][y] = new Arbre();
-                arbrePlante = true;
-            } else {
-                x = 0;
-                y = 0;
-            }
+        int coordonneX = new Random().nextInt((arbrePere.getRayonDispersion()) + arbrePere.getRayonDispersion() ) - arbrePere.getRayonDispersion();
+        int coordonneY = new Random().nextInt((arbrePere.getRayonDispersion()) + arbrePere.getRayonDispersion() ) - arbrePere.getRayonDispersion();
+
+        if (arbrePere.getX()+coordonneX > tableauArbre.length - 1) {
+            coordonneX = arbrePere.getX() + coordonneX - tableauArbre.length;
+        } else if (arbrePere.getX() + coordonneX < 0) {
+            coordonneX = arbrePere.getX() + coordonneX + tableauArbre.length;
         }
+
+        if (arbrePere.getY()+coordonneY > tableauArbre.length - 1) {
+            coordonneY = arbrePere.getY() + coordonneY - tableauArbre.length;
+        } else if (arbrePere.getY() + coordonneY < 0) {
+            coordonneY = arbrePere.getX() + coordonneY + tableauArbre.length;
+        }
+        if (tableauArbre[coordonneX][coordonneY] == null) {
+
+            Arbre arbreFils = new Arbre(coordonneX,
+                     coordonneY,
+                     arbrePere.getRayonCompetition(),
+                     arbrePere.getRayonDispersion(),
+                     arbrePere.getEsperanceVie(),
+                     arbrePere.getIntensiteCompetition(),
+                     arbrePere.getChanceReproduction());
+
+            addArbre(arbreFils);
+         }
     }
 
     public void listToString() {
